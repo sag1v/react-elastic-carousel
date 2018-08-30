f8d7fd5b6c49c8a3986b0c906b80da62ddd870ee
Sagiv Ben Giat <Sagiv@silverbyte.local>
2018-08-30 16:08:15 +0300

    init create-react-library@2.6.1

---
+++ example/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+import ExampleComponent from 'react-elastic-carousel'
+
+export default class App extends Component {
+  render () {
+    return (
+      <div>
+        <ExampleComponent text='Modern React component module' />
+      </div>
+    )
+  }
+}
