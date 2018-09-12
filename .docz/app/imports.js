0c73481d60085914776ff422c3f74e6dced13997
Sagiv Ben Giat <Sagiv@silverbyte.local>
2018-09-12 16:04:59 +0300

    renderArrow example

--- .docz/app/imports.js
+++ .docz/app/imports.js
@@ -9,30 +9,32 @@ export const imports = {
     import(/* webpackPrefetch: true, webpackChunkName: "mdx-focus-on-select" */ 'mdx/focusOnSelect.mdx'),
   'mdx/goTo.mdx': () =>
     import(/* webpackPrefetch: true, webpackChunkName: "mdx-go-to" */ 'mdx/goTo.mdx'),
-  'mdx/index.mdx': () =>
-    import(/* webpackPrefetch: true, webpackChunkName: "mdx-index" */ 'mdx/index.mdx'),
   'mdx/isRTL.mdx': () =>
     import(/* webpackPrefetch: true, webpackChunkName: "mdx-is-rtl" */ 'mdx/isRTL.mdx'),
+  'mdx/index.mdx': () =>
+    import(/* webpackPrefetch: true, webpackChunkName: "mdx-index" */ 'mdx/index.mdx'),
   'mdx/initialFirstItem.mdx': () =>
     import(/* webpackPrefetch: true, webpackChunkName: "mdx-initial-first-item" */ 'mdx/initialFirstItem.mdx'),
   'mdx/itemPadding.mdx': () =>
     import(/* webpackPrefetch: true, webpackChunkName: "mdx-item-padding" */ 'mdx/itemPadding.mdx'),
   'mdx/itemPosition.mdx': () =>
     import(/* webpackPrefetch: true, webpackChunkName: "mdx-item-position" */ 'mdx/itemPosition.mdx'),
-  'mdx/itemsToScroll.mdx': () =>
-    import(/* webpackPrefetch: true, webpackChunkName: "mdx-items-to-scroll" */ 'mdx/itemsToScroll.mdx'),
   'mdx/itemsToShow.mdx': () =>
     import(/* webpackPrefetch: true, webpackChunkName: "mdx-items-to-show" */ 'mdx/itemsToShow.mdx'),
+  'mdx/itemsToScroll.mdx': () =>
+    import(/* webpackPrefetch: true, webpackChunkName: "mdx-items-to-scroll" */ 'mdx/itemsToScroll.mdx'),
+  'mdx/onNextStart.mdx': () =>
+    import(/* webpackPrefetch: true, webpackChunkName: "mdx-on-next-start" */ 'mdx/onNextStart.mdx'),
   'mdx/onNextEnd.mdx': () =>
     import(/* webpackPrefetch: true, webpackChunkName: "mdx-on-next-end" */ 'mdx/onNextEnd.mdx'),
   'mdx/onPrevEnd.mdx': () =>
     import(/* webpackPrefetch: true, webpackChunkName: "mdx-on-prev-end" */ 'mdx/onPrevEnd.mdx'),
-  'mdx/onNextStart.mdx': () =>
-    import(/* webpackPrefetch: true, webpackChunkName: "mdx-on-next-start" */ 'mdx/onNextStart.mdx'),
   'mdx/onResize.mdx': () =>
     import(/* webpackPrefetch: true, webpackChunkName: "mdx-on-resize" */ 'mdx/onResize.mdx'),
   'mdx/onPrevStart.mdx': () =>
     import(/* webpackPrefetch: true, webpackChunkName: "mdx-on-prev-start" */ 'mdx/onPrevStart.mdx'),
+  'mdx/renderArrow.mdx': () =>
+    import(/* webpackPrefetch: true, webpackChunkName: "mdx-render-arrow" */ 'mdx/renderArrow.mdx'),
   'mdx/slideNext.mdx': () =>
     import(/* webpackPrefetch: true, webpackChunkName: "mdx-slide-next" */ 'mdx/slideNext.mdx'),
 }
---
+++ src/docs/components/Button.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+export default styled.div`
+  font-size: 2.5em;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+  user-select: none;
+`;
