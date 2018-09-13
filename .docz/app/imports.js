549157853cc136f17a3f780386543459fad186bc
sag1v <sagiv.bengiat@gmail.com>
2018-09-13 10:10:41 +0300

    normilize docz styles

--- .docz/app/imports.js
+++ .docz/app/imports.js
@@ -1,40 +1,40 @@
 export const imports = {
   'mdx/breakpoints.mdx': () =>
     import(/* webpackPrefetch: true, webpackChunkName: "mdx-breakpoints" */ 'mdx/breakpoints.mdx'),
-  'mdx/easing.mdx': () =>
-    import(/* webpackPrefetch: true, webpackChunkName: "mdx-easing" */ 'mdx/easing.mdx'),
-  'mdx/enableAutoPlay.mdx': () =>
-    import(/* webpackPrefetch: true, webpackChunkName: "mdx-enable-auto-play" */ 'mdx/enableAutoPlay.mdx'),
   'mdx/focusOnSelect.mdx': () =>
     import(/* webpackPrefetch: true, webpackChunkName: "mdx-focus-on-select" */ 'mdx/focusOnSelect.mdx'),
+  'mdx/enableAutoPlay.mdx': () =>
+    import(/* webpackPrefetch: true, webpackChunkName: "mdx-enable-auto-play" */ 'mdx/enableAutoPlay.mdx'),
+  'mdx/index.mdx': () =>
+    import(/* webpackPrefetch: true, webpackChunkName: "mdx-index" */ 'mdx/index.mdx'),
   'mdx/goTo.mdx': () =>
     import(/* webpackPrefetch: true, webpackChunkName: "mdx-go-to" */ 'mdx/goTo.mdx'),
   'mdx/isRTL.mdx': () =>
     import(/* webpackPrefetch: true, webpackChunkName: "mdx-is-rtl" */ 'mdx/isRTL.mdx'),
-  'mdx/index.mdx': () =>
-    import(/* webpackPrefetch: true, webpackChunkName: "mdx-index" */ 'mdx/index.mdx'),
   'mdx/initialFirstItem.mdx': () =>
     import(/* webpackPrefetch: true, webpackChunkName: "mdx-initial-first-item" */ 'mdx/initialFirstItem.mdx'),
-  'mdx/itemPadding.mdx': () =>
-    import(/* webpackPrefetch: true, webpackChunkName: "mdx-item-padding" */ 'mdx/itemPadding.mdx'),
   'mdx/itemPosition.mdx': () =>
     import(/* webpackPrefetch: true, webpackChunkName: "mdx-item-position" */ 'mdx/itemPosition.mdx'),
+  'mdx/itemPadding.mdx': () =>
+    import(/* webpackPrefetch: true, webpackChunkName: "mdx-item-padding" */ 'mdx/itemPadding.mdx'),
   'mdx/itemsToShow.mdx': () =>
     import(/* webpackPrefetch: true, webpackChunkName: "mdx-items-to-show" */ 'mdx/itemsToShow.mdx'),
   'mdx/itemsToScroll.mdx': () =>
     import(/* webpackPrefetch: true, webpackChunkName: "mdx-items-to-scroll" */ 'mdx/itemsToScroll.mdx'),
+  'mdx/easing.mdx': () =>
+    import(/* webpackPrefetch: true, webpackChunkName: "mdx-easing" */ 'mdx/easing.mdx'),
   'mdx/onNextStart.mdx': () =>
     import(/* webpackPrefetch: true, webpackChunkName: "mdx-on-next-start" */ 'mdx/onNextStart.mdx'),
   'mdx/onNextEnd.mdx': () =>
     import(/* webpackPrefetch: true, webpackChunkName: "mdx-on-next-end" */ 'mdx/onNextEnd.mdx'),
+  'mdx/onPrevStart.mdx': () =>
+    import(/* webpackPrefetch: true, webpackChunkName: "mdx-on-prev-start" */ 'mdx/onPrevStart.mdx'),
   'mdx/onPrevEnd.mdx': () =>
     import(/* webpackPrefetch: true, webpackChunkName: "mdx-on-prev-end" */ 'mdx/onPrevEnd.mdx'),
   'mdx/onResize.mdx': () =>
     import(/* webpackPrefetch: true, webpackChunkName: "mdx-on-resize" */ 'mdx/onResize.mdx'),
-  'mdx/onPrevStart.mdx': () =>
-    import(/* webpackPrefetch: true, webpackChunkName: "mdx-on-prev-start" */ 'mdx/onPrevStart.mdx'),
-  'mdx/renderArrow.mdx': () =>
-    import(/* webpackPrefetch: true, webpackChunkName: "mdx-render-arrow" */ 'mdx/renderArrow.mdx'),
   'mdx/slideNext.mdx': () =>
     import(/* webpackPrefetch: true, webpackChunkName: "mdx-slide-next" */ 'mdx/slideNext.mdx'),
+  'mdx/renderArrow.mdx': () =>
+    import(/* webpackPrefetch: true, webpackChunkName: "mdx-render-arrow" */ 'mdx/renderArrow.mdx'),
 }
