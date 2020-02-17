83ccf36add7e7be867537392f566ea94c3f33ec9
Sanish <skumar1@tila.com>
2020-02-17 14:18:02 +0530

    :bug: fix attrs bug

--- src/react-elastic-carousel/components/styled/ItemWrapper.js
+++ src/react-elastic-carousel/components/styled/ItemWrapper.js
@@ -1,10 +1,8 @@
 import styled from "styled-components";
 
-export default styled.div.attrs(({ padding, width, height }) => {
-  return {
-    style: { padding, width, height }
-  };
-})`
+export default styled.div.attrs(({ style }) => ({
+  style
+}))`
   box-sizing: border-box;
   display: flex;
   overflow: hidden;
