47d51c3d0371ce67fa3afd181f31bc95bda12b5a
sagiv.bengiat <sagiv.bengiat@appsflyer.com>
2020-05-28 18:23:42 +0300

    remove demo files from repo

--- demo/styles-a9a040cfa545425c4ed3.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[24],{oKgo:function(o,n,w){}}]);
-//# sourceMappingURL=styles-a9a040cfa545425c4ed3.js.map
--- demo/styles-f334a9840507c184c189.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[25],{oKgo:function(o,n,w){}}]);
-//# sourceMappingURL=styles-f334a9840507c184c189.js.map
