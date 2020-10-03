618180b8e15e6422da7e3555788f159b083c5a94
sagiv.bengiat <sagiv.bengiat@appsflyer.com>
2020-10-03 18:39:09 +0300

    fixed proptyoes of example

--- src/docs/components/ItemWithLink.js
+++ src/docs/components/ItemWithLink.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 import Item from "./SimpleItem";
 
 const ItemWithLink = ({ tabIndex, text }) => (
@@ -8,5 +9,10 @@ const ItemWithLink = ({ tabIndex, text }) => (
     </a>
   </Item>
 );
+
+ItemWithLink.propTypes = {
+  text: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+  tabIndex: PropTypes.oneOfType([PropTypes.number, PropTypes.string])
+};
 
 export default ItemWithLink;
