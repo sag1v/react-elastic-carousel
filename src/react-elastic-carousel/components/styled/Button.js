f4820d2a14dce822debe78e108e169c8ba2a22e4
sagiv.bengiat <sagiv.bengiat@appsflyer.com>
2020-12-03 15:34:25 +0200

    remove padding for buttons resolve #124

--- src/react-elastic-carousel/components/styled/Button.js
+++ src/react-elastic-carousel/components/styled/Button.js
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 
 export default styled.button.attrs(({ type = "button" }) => ({ type }))`
+  box-sizing: border-box;
   transition: all 0.3s ease;
   font-size: 1.6em;
   background-color: rgba(103, 58, 183, 0.1);
@@ -8,6 +9,7 @@ export default styled.button.attrs(({ type = "button" }) => ({ type }))`
   box-shadow: 0 0 2px 0px #333;
   border-radius: 50%;
   border: none;
+  padding: 0;
   width: 50px;
   height: 50px;
   min-width: 50px;
